efb30ccbbdb59c55719c0f473a3529a65644a846
chrisdew <cmsdew@gmail.com>
2013-09-20 12:50:59 +0100

    got mouse events working in ngMap directive

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('mapApp', [])
+var app = angular.module('mapApp', [])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
@@ -11,3 +11,25 @@ angular.module('mapApp', [])
         redirectTo: '/'
       });
   });
+
+app.directive("ngMap", function(){
+  return {
+    restrict: "A",
+    link: function(scope, element){
+      var ctx = element[0].getContext('2d');
+      element.bind('mousedown', function(event){
+        console.log('mousedown');
+      });
+      element.bind('mousemove', function(event){
+        console.log('mousemove');
+      });
+      element.bind('mouseup', function(event){
+        console.log('mouseup');
+      });
+      // canvas reset
+      function reset(){
+        element[0].width = element[0].width;
+      }
+    }
+  };
+});
