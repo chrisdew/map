7016b7dacb90785e1d62bc8976b3a322e7af69a4
chrisdew <cmsdew@gmail.com>
2013-09-20 12:00:06 +0100

    added yesterday evening's work

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('mapApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
